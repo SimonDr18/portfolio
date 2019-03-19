d486a89ca30495762cdea4e0f431c8e0c3bfe93a
SimonDr18 <simon.drieux@orange.fr>
2019-03-19 17:16:38 -0400

    [MOD] Ajout metadata close #1

--- wp-content/themes/materialis/single.php
+++ wp-content/themes/materialis/single.php
@@ -7,6 +7,15 @@
                     <?php
                     if (have_posts()):
                         while (have_posts()):
+					
+							$champsMeta = get_post_custom();?>
+							<ul class = "post-meta" style="list-style: none">
+								<li id="oeuvre-technologie" style="color : red">Technologies : <span class="post-meta-key"><?php echo $champsMeta['Technologie'][0];?></span></li>
+								<li id="oeuvre-interoperabilite" style="color : yellow">Interopérabilité : <span class="post-meta-key"><?php echo $champsMeta['Interoperabilite'][0];?></span></li>
+								<li id="oeuvre-auteur" style="color : green">Auteur(s): <span class="post-meta-key"><?php echo $champsMeta['Auteur'][0];?></span></li>
+								<li id="oeuvre-date" style="color : blue">Date de création : <span class="post-meta-key"><?php echo $champsMeta['Date'][0];?></span></li>
+							</ul>
+					<?php
                             the_post();
                             get_template_part('template-parts/content', 'single');
                         endwhile;
